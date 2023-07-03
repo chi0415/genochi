f602db931113800dd0b7068c640a93a0746a87d1
chi0415 <chikama83abc@gmail>
2023-07-03 18:28:27 +0900

    [ruby] index

---
+++ ruby/case3.rb
@@ -0,0 +1,7 @@
+wallet = 300
+case
+when wallet >= 500
+  puts "モカにホイップを追加"
+when wallet >= 300
+  puts "カフェラテ"
+end
---
+++ ruby/elsif.rb
@@ -0,0 +1,8 @@
+season = "春"
+if season == "春"
+  puts "アイスを買って行こう"
+elsif season == "夏"
+  puts "かき氷を買って行こう"
+else
+  puts "肉まんを買って行こう"
+end
